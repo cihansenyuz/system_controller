d84a0e6669f7978ad5e9bbdf7823a603c3607683
cihansenyuz <cihansenyuz@hotmail.com>
2024-09-13 14:05:56 +0300

    implement finding file logics

--- modules/sw_file_manager.py
+++ modules/sw_file_manager.py
@@ -4,6 +4,8 @@ from PySide6.QtCore import Signal
 class SwFileManager(FileBrowser):
     foundSwFile = Signal(bool)
     foundCusData = Signal(bool)
+    foundOemFile = Signal(bool)
+    foundPidFile = Signal(bool)
 
     def __init__(self, rootDirectory):
         super().__init__(rootDirectory)
@@ -15,35 +17,40 @@ class SwFileManager(FileBrowser):
     def createTargetDirectories(self):
         self.targetSwDir = self.rootDirectory + self.projectName + self.osSeperator + "USBDEN_YUKLEME" + self.osSeperator + "BIRINCI_USB"
         self.targetPidDir = self.rootDirectory + self.projectName + self.osSeperator + "PROJECT_ID_YUKLEME"
-        self.targetOemDir = self.rootDirectory + self.projectName + self.osSeperator + "OEM_YUKLEME"
+        self.targetOemDir = self.rootDirectory + self.projectName + self.osSeperator + "OEM_YUKLEME\GRUNDIG_NONFARFIELD"
 
     def setProjectName(self, name):
         self.projectName = name
         self.createTargetDirectories()
 
     def findUsbSwImage(self):
-        fileList = self.getFileList()
-        self.swFilePath = self.currentDirectory + self.osSeperator + fileList[-1]
+        fileList = self.getFileList(self.targetSwDir)
+        self.swFilePath = self.targetSwDir + self.osSeperator + fileList[-1]
         if fileList[-1] == "upgrade_image_no_tvcertificate.pkg":
             self.foundSwFile.emit(True)
         else:
             self.foundSwFile.emit(False)
     
     def findOemFile(self):
-        pass
+        fileList = self.getFileList(self.targetOemDir)
+        self.oemPath = self.targetOemDir + self.osSeperator + fileList[-1]
+        if fileList[-1] == "upgrade_image_oem.pkg":
+            self.foundOemFile.emit(True)
+        else:
+            self.foundOemFile.emit(False)
 
     def findCusDataFile(self):
-        fileList = self.getFileList()
-        self.cusDataPath = self.currentDirectory + self.osSeperator + fileList[0]
+        fileList = self.getFileList(self.targetSwDir)
+        self.cusDataPath = self.targetSwDir + self.osSeperator + fileList[0]
         if fileList[0] == "upgrade_image_cusdata.pkg":
             self.foundCusData.emit(True)
         else:
             self.foundCusData.emit(False)
 
     def findPidFile(self):
-        fileList = self.getFileList()
-        self.cusDataPath = self.currentDirectory + self.osSeperator + fileList[0]
+        fileList = self.getFileList(self.targetPidDir)
+        self.pidPath = self.targetPidDir + self.osSeperator + fileList[0]
         if fileList[0] == "upgrade_image_cusdata.pkg":
-            self.foundCusData.emit(True)
+            self.foundPidFile.emit(True)
         else:
-            self.foundCusData.emit(False)
+            self.foundPidFile.emit(False)
--- ui_compiled/ui_image_creator_screen.py
+++ ui_compiled/ui_image_creator_screen.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'image_creator_screen.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.7.2
+## Created by: Qt User Interface Compiler version 6.6.1
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -15,9 +15,9 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QComboBox, QGroupBox, QHBoxLayout,
-    QLabel, QLineEdit, QPlainTextEdit, QPushButton,
-    QSizePolicy, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox, QGroupBox,
+    QHBoxLayout, QLabel, QLineEdit, QPlainTextEdit,
+    QPushButton, QSizePolicy, QVBoxLayout, QWidget)
 
 class Ui_imageCreatorWindow(object):
     def setupUi(self, imageCreatorWindow):
@@ -28,23 +28,22 @@ class Ui_imageCreatorWindow(object):
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.groupBox_3 = QGroupBox(imageCreatorWindow)
         self.groupBox_3.setObjectName(u"groupBox_3")
+        font = QFont()
+        font.setPointSize(12)
+        self.groupBox_3.setFont(font)
         self.verticalLayout = QVBoxLayout(self.groupBox_3)
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.infoMessages = QPlainTextEdit(self.groupBox_3)
-        self.infoMessages.setObjectName(u"infoMessages")
-
-        self.verticalLayout.addWidget(self.infoMessages)
-
         self.horizontalLayout_2 = QHBoxLayout()
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
         self.label = QLabel(self.groupBox_3)
         self.label.setObjectName(u"label")
+        self.label.setFont(font)
 
         self.horizontalLayout_2.addWidget(self.label)
 
         self.projectNameLineEdit = QLineEdit(self.groupBox_3)
         self.projectNameLineEdit.setObjectName(u"projectNameLineEdit")
-        sizePolicy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Minimum)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.projectNameLineEdit.sizePolicy().hasHeightForWidth())
@@ -54,33 +53,63 @@ class Ui_imageCreatorWindow(object):
 
         self.findButton = QPushButton(self.groupBox_3)
         self.findButton.setObjectName(u"findButton")
-        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Minimum)
+        sizePolicy1 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
         sizePolicy1.setHorizontalStretch(0)
         sizePolicy1.setVerticalStretch(0)
         sizePolicy1.setHeightForWidth(self.findButton.sizePolicy().hasHeightForWidth())
         self.findButton.setSizePolicy(sizePolicy1)
+        self.findButton.setFont(font)
 
         self.horizontalLayout_2.addWidget(self.findButton)
 
+        self.horizontalLayout_2.setStretch(0, 1)
+        self.horizontalLayout_2.setStretch(1, 1)
+        self.horizontalLayout_2.setStretch(2, 1)
 
         self.verticalLayout.addLayout(self.horizontalLayout_2)
 
-        self.verticalLayout.setStretch(0, 6)
-        self.verticalLayout.setStretch(1, 1)
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.label_2 = QLabel(self.groupBox_3)
+        self.label_2.setObjectName(u"label_2")
+
+        self.horizontalLayout_3.addWidget(self.label_2)
+
+        self.lineEdit = QLineEdit(self.groupBox_3)
+        self.lineEdit.setObjectName(u"lineEdit")
+
+        self.horizontalLayout_3.addWidget(self.lineEdit)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_3)
+
+        self.checkBox = QCheckBox(self.groupBox_3)
+        self.checkBox.setObjectName(u"checkBox")
+        self.checkBox.setLayoutDirection(Qt.LeftToRight)
+
+        self.verticalLayout.addWidget(self.checkBox)
+
+        self.infoMessages = QPlainTextEdit(self.groupBox_3)
+        self.infoMessages.setObjectName(u"infoMessages")
+
+        self.verticalLayout.addWidget(self.infoMessages)
+
 
         self.horizontalLayout.addWidget(self.groupBox_3)
 
         self.groupBox = QGroupBox(imageCreatorWindow)
         self.groupBox.setObjectName(u"groupBox")
+        self.groupBox.setFont(font)
         self.verticalLayout_3 = QVBoxLayout(self.groupBox)
         self.verticalLayout_3.setObjectName(u"verticalLayout_3")
         self.usbDevicesBox = QComboBox(self.groupBox)
         self.usbDevicesBox.setObjectName(u"usbDevicesBox")
-        sizePolicy2 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Minimum)
+        sizePolicy2 = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Minimum)
         sizePolicy2.setHorizontalStretch(0)
         sizePolicy2.setVerticalStretch(0)
         sizePolicy2.setHeightForWidth(self.usbDevicesBox.sizePolicy().hasHeightForWidth())
         self.usbDevicesBox.setSizePolicy(sizePolicy2)
+        self.usbDevicesBox.setFont(font)
 
         self.verticalLayout_3.addWidget(self.usbDevicesBox)
 
@@ -88,6 +117,7 @@ class Ui_imageCreatorWindow(object):
         self.refreshButton.setObjectName(u"refreshButton")
         sizePolicy1.setHeightForWidth(self.refreshButton.sizePolicy().hasHeightForWidth())
         self.refreshButton.setSizePolicy(sizePolicy1)
+        self.refreshButton.setFont(font)
 
         self.verticalLayout_3.addWidget(self.refreshButton)
 
@@ -99,6 +129,7 @@ class Ui_imageCreatorWindow(object):
         self.prepareButton.setEnabled(False)
         sizePolicy1.setHeightForWidth(self.prepareButton.sizePolicy().hasHeightForWidth())
         self.prepareButton.setSizePolicy(sizePolicy1)
+        self.prepareButton.setFont(font)
 
         self.horizontalLayout.addWidget(self.prepareButton)
 
@@ -113,9 +144,11 @@ class Ui_imageCreatorWindow(object):
 
     def retranslateUi(self, imageCreatorWindow):
         imageCreatorWindow.setWindowTitle(QCoreApplication.translate("imageCreatorWindow", u"Form", None))
-        self.groupBox_3.setTitle(QCoreApplication.translate("imageCreatorWindow", u"Bilgi Paneli", None))
+        self.groupBox_3.setTitle(QCoreApplication.translate("imageCreatorWindow", u"Dosya Se\u00e7im Paneli", None))
         self.label.setText(QCoreApplication.translate("imageCreatorWindow", u"Image'\u0131 haz\u0131rlanacak proje:", None))
-        self.findButton.setText(QCoreApplication.translate("imageCreatorWindow", u"Versiyon Bul", None))
+        self.findButton.setText(QCoreApplication.translate("imageCreatorWindow", u"USB Image Bul", None))
+        self.label_2.setText(QCoreApplication.translate("imageCreatorWindow", u"ProjectID:", None))
+        self.checkBox.setText(QCoreApplication.translate("imageCreatorWindow", u"4'l\u00fc paket haz\u0131rlans\u0131n m\u0131?", None))
         self.groupBox.setTitle(QCoreApplication.translate("imageCreatorWindow", u"USB Se\u00e7imi", None))
         self.refreshButton.setText(QCoreApplication.translate("imageCreatorWindow", u"Yenile", None))
         self.prepareButton.setText(QCoreApplication.translate("imageCreatorWindow", u"Haz\u0131rla", None))
