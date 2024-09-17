265d91e09fb574eb24696c97c0692bcb471a697b
cihansenyuz <cihansenyuz@hotmail.com>
2024-09-17 16:29:03 +0300

    add dev mode flag

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from PySide6.QtWidgets import QApplication
 from screens.main_window import MainWindow
-
 import sys
+import argparse
+
+# Add argument parser
+parser = argparse.ArgumentParser(description="Launch the application")
+parser.add_argument("--dev", action="store_true", help="Launch in developer mode")
+args = parser.parse_args()
 
 app = QApplication(sys.argv)
 ui = MainWindow()
 
-#ui.showFullScreen()
-ui.show()
+if args.dev:
+    ui.show()
+else:
+    ui.showFullScreen()
+
 sys.exit(app.exec())
